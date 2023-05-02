6b58b5d02f93e53c3b35ae60b3b7bd8d8b9bcc4e
realdashiedash <31542077+dashiedash@users.noreply.github.com>
2023-05-02 16:14:08 +0800

    Create header component

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,3 @@
+  <h1 class="text-3xl font-bold underline">
+    If you see this, then the header works.
+  </h1>
--- resources/views/layout/index.blade.php
+++ resources/views/layout/index.blade.php
@@ -8,9 +8,7 @@
 </head>
 
 <body>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+  <x-header></x-header>
 </body>
 
 </html>
