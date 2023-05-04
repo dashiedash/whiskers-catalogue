3146c7eed60b29fcb7fe15468d36a88ccf12907e
realdashiedash <31542077+dashiedash@users.noreply.github.com>
2023-05-04 17:25:08 +0800

    Add content to footer

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,31 @@
+<footer {{ $attributes->class('bg-slate-100') }}>
+  <div class="container grid lg:grid-cols-12 auto-cols-auto gap-3 max-w-screen-xl mx-auto sm:p-7 p-3">
+    {{-- Footer Logo --}}
+    <div class="col-span-3">
+        <div class="">
+            Logo
+        </div>
+      <p>
+        123 Bentong St.
+        <br>
+        Damulag, Santa Clause
+        <br>
+        5555 Zumbales
+        <br>
+        Philippines
+      </p>
+    </div>
+    <div class="col-span-3">
+        Socials
+    </div>
+    <div class="col-span-3">
+        Links
+    </div>
+    <div class="col-span-3">
+        Etc,
+    </div>
+    <div class="col-span-12">
+        Copyright
+    </div>
+  </div>
+</footer>
