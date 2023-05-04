a4fe89f8e3ccf04b7d5883c5a347c286022ff71d
realdashiedash <31542077+dashiedash@users.noreply.github.com>
2023-05-04 17:25:46 +0800

    Forgot to stage this file lol

--- resources/views/layout/index.blade.php
+++ resources/views/layout/index.blade.php
@@ -6,6 +6,7 @@
 <body class="bg-slate-100">
   <x-header class="" />
   <x-hero class="" />
+  <x-footer class="bg-slate-300" />
 </body>
 
 <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
