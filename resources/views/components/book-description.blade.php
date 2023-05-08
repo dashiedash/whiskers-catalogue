d83f8115e28a3794ed00a50dfe63c1b61be12a2f
realdashiedash <31542077+dashiedash@users.noreply.github.com>
2023-05-08 17:17:46 +0800

    Set width for image
    Didn't work :<

--- resources/views/components/book-description.blade.php
+++ resources/views/components/book-description.blade.php
@@ -1,5 +1,10 @@
 <main>
-    <div class="container">
-        
+  <div class="container">
+    <div class="flex">
+      <div class="w-1/8">
+        <img class="" src="\images\default-cover.png" alt="" />
+      </div>
+      <h3>Book Title</h3>
     </div>
+  </div>
 </main>
