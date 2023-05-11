a5e557813776e3f30dca1a703d2ed325276aa9bd
realdashiedash <31542077+dashiedash@users.noreply.github.com>
2023-05-11 16:38:30 +0800

    Add links to single book display.

--- resources/views/components/book-table.blade.php
+++ resources/views/components/book-table.blade.php
@@ -2,14 +2,18 @@
   <div class="container my-7 mx-auto max-w-screen-xl rounded-lg">
     <div class="flex flex-wrap justify-evenly">
       @foreach ($books as $book)
-        <div class="m-3 flex md:w-2/5 w-full bg-white p-7">
+        <div class="m-3 flex w-full bg-white p-7 md:w-2/5">
           <div class="w-1/4">
             <img class="w-full" src="\images\default-cover.png" alt="" />
           </div>
           <div class="w-3/4 px-3">
-            <h3 class="text-xl font-bold">{{ $book->title }}</h3>
-            <p class="text-sm italic text-slate-600">{{ $book->subtitle }}</p>
-            <p class="font-bold">{{ $book->author_first_name }} {{ $book->author_last_name }}</p>
+            <a href="book/{{ $book['id'] }}">
+              <h3 class="text-xl font-bold">
+                {{ $book->title }}
+              </h3>
+              <p class="text-sm italic text-slate-600">{{ $book->subtitle }}</p>
+            </a>
+            <p class="font-bold my-2">{{ $book->author_first_name }} {{ $book->author_last_name }}</p>
             <p class="my-3 font-bold">$ {{ $book->price }}</p>
           </div>
         </div>
